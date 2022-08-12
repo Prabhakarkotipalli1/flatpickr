fe04c252061b27e4eb1d53f789595e00f8af5b69
Prabhakarkotipalli1 <prabhakar@digitaldots.ai>
2022-08-12 19:33:03 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,32 +4,30 @@ import "flatpickr/dist/themes/material_green.css";
 import moment from "moment";
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.datePicker = React.createRef();
-    this.state = {
-      value: "2021-05-04T14:00:00.938Z"
-    };
-  }
-  onChange(selectedDates, dateStr, instance) {
-    console.log(selectedDates);
-    this.setState({ value: new Date(selectedDates[0]).toLocaleString() });
-  }
-  componentDidMount() {
-    console.log("New Date: ", new Date("2021-05/04T14:00:00.938Z"));
-    flatpickr(this.datePicker.current, {
-      onChange: this.onChange.bind(this),
-      defaultDate: this.state.value,
-      enableTime: true,
-      allowInput: true,
-      allowInvalidPreload: false,
-      time_24hr: false
-      //   formatDate: (date, format) => {
-      //     return date.toLocaleString()
-      // }
-    });
-  }
-  render() {
-    return <input type="date" ref={this.datePicker} name={this.state.value} />;
-  }
+    constructor(props) {
+        super(props);
+        this.datePicker = React.createRef();
+        this.state = {
+            value: "2021-05-04T14:00:00.938Z"
+        };
+    }
+    onChange(selectedDates, dateStr, instance) {
+        console.log(selectedDates);
+    }
+    componentDidMount() {
+        flatpickr(this.datePicker.current, {
+            onChange: this.onChange.bind(this),
+            defaultDate: this.state.value,
+            enableTime: true,
+            allowInput: true,
+            allowInvalidPreload: false,
+            time_24hr: false
+            //   formatDate: (date, format) => {
+            //     return date.toLocaleString()
+            // }
+        });
+    }
+    render() {
+        return <input type="date" ref={this.datePicker} />;
+    }
 }
